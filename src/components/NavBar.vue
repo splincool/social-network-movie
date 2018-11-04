0e95f8625ef31bbcd3c19f11acc153e60ea2a187
Alena Borisova <41994884+Angloya@users.noreply.github.com>
2018-11-04 21:52:56 +0300

    fixed bugs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,12 +13,6 @@
         <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
         <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
       </b-nav-form>
-      <b-nav-item-dropdown text="Lang" right>
-        <b-dropdown-item href="#">EN</b-dropdown-item>
-        <b-dropdown-item href="#">ES</b-dropdown-item>
-        <b-dropdown-item href="#">RU</b-dropdown-item>
-        <b-dropdown-item href="#">FA</b-dropdown-item>
-      </b-nav-item-dropdown>
       <b-nav-item-dropdown v-if="user" right>
         <!-- Using button-content slot -->
         <template v-if="user.name" slot="button-content">
