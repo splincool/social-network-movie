88995738d7e8bf01d77e1362122e0cf418923e73
splincool <mak.ivanov@gmail.com>
2018-11-19 22:47:22 +0300

    added a config file

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // options...
+    baseUrl: process.env.NODE_ENV === 'production' ? 'projects/movie-network/' : '/',
+  
+    // Other options include:
+    outputDir: undefined,
+    assetsDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: undefined,
+    parallel: undefined,
+    css: undefined
+  }
